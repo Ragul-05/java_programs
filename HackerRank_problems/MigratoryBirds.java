38b9907536d2c923c8e91867a54c0641bc2cea58
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2024-09-09 19:21:20 +0530

    Hackerrank Problem

---
+++ HackerRank_problems/MigratoryBirds.java
@@ -0,0 +1,70 @@
+package HackerRank_problems;
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'migratoryBirds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int migratoryBirds(List<Integer> arr) {
+    // Write your code here
+       int[] frequency = new int[6]; 
+
+       
+        for (int birdType : arr) {
+            frequency[birdType]++;
+        }
+
+        int maxFrequency = 0;
+        int birdWithMaxFrequency = 0;
+
+       
+        for (int i = 1; i <= 5; i++) {
+            if (frequency[i] > maxFrequency) {
+                maxFrequency = frequency[i];
+                birdWithMaxFrequency = i;
+            }
+        }
+
+        return birdWithMaxFrequency;
+    }
+}
+
+public class MigratoryBirds {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < arrCount; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        int result = Result.migratoryBirds(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
