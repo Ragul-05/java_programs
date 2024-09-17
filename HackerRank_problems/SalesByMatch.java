1a440cb727d571728b1191d6505745f5c6c7898c
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2024-09-17 20:51:41 +0530

    Hackerrank Problem

---
+++ HackerRank_problems/SalesByMatch.java
@@ -0,0 +1,75 @@
+package HackerRank_problems;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+    // Write your code here
+     
+        Collections.sort(ar);
+        int res = 0;
+        int i = 0;
+ 
+        while (i < n) {
+             
+            
+            int number = ar.get(i);
+            int count = 1;
+            i++;
+ 
+            while (i < n && ar.get(i) == number) {
+                count++;
+                i++;
+            }
+             
+            if (count >= 2) {
+                res = res + count / 2; 
+            }
+        }
+        return res; 
+    }
+
+    }
+
+public class SalesByMatch {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> ar = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arTemp[i]);
+            ar.add(arItem);
+        }
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
