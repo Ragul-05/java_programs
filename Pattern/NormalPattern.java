33956986dde75a872c90c03029d7f3b8f7cc9f86
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2024-09-26 21:56:03 +0530

    Pattern Problem -->1

---
+++ Pattern/NormalPattern.java
@@ -0,0 +1,16 @@
+package Pattern;
+import java.util.*;
+public class NormalPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+    int n = sc.nextInt();
+
+    for(int i = 0;i < n; i ++){
+        for(int j = 0; j < n ;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    
+    }
+}
