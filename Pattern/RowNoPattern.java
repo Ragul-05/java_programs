bc2168e810d5282d9232584b42e168587e36fc4a
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2024-09-26 21:57:11 +0530

    Pattern Problem -->2

---
+++ Pattern/RowNoPattern.java
@@ -0,0 +1,18 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class RowNoPattern {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    int n = sc.nextInt();
+
+    for(int i = 1;i <= n; i ++){
+        for(int j = 1; j <= i ;j++){
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+    
+    }
+}
