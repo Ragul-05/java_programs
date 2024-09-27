62127be5570269b2bf94d089c1a7dfba92cca278
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2024-09-27 21:35:28 +0530

    Pattern Problem -->12

---
+++ Pattern/IncrementalPattern.java
@@ -0,0 +1,20 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class IncrementalPattern {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    int n = sc.nextInt();
+    int num =1;
+    for(int i = 1;i <= n; i ++){
+        for(int j = 1; j <= i ;j++){
+            System.out.print(" ");
+            System.out.print(num);
+            num +=1;
+        }
+        System.out.println();
+    }
+    
+    }
+}
