43e02b8dc304ad1e18b26a421f8bddd6af132abf
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2024-10-13 08:43:43 +0530

    Factorial of a number

---
+++ Data_Structure/Recursion/Factorial.java
@@ -0,0 +1,19 @@
+package Data_Structure.Recursion;
+
+import java.util.Scanner;
+
+public class Factorial {
+    static int factorial(int n) {
+        if(n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(factorial(n));
+    }
+}
+
