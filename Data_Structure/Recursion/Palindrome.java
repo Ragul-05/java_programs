0894f7fc7f19230e21d225acae1b7106c02f4e9b
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2024-10-13 09:21:56 +0530

    Palindrome Recursion

---
+++ Data_Structure/Recursion/Palindrome.java
@@ -0,0 +1,21 @@
+package Data_Structure.Recursion;
+
+public class Palindrome {
+    static boolean palindrome(int i, String s) {
+        
+        if (i >= s.length() / 2) return true;
+
+        
+        if (s.charAt(i) != s.charAt(s.length() - i - 1)) return false;
+
+       
+        return palindrome(i + 1, s);
+    }
+
+    public static void main(String[] args) {
+        // Example string
+        String s = "madam";
+        System.out.println(palindrome(0, s));
+    }
+}
+
