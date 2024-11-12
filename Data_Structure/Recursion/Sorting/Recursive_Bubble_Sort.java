f46729743dfd165a0565a1b05d9ade1dce08c29b
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2024-11-12 08:53:29 +0530

    Added the Recursion of bubble sort file

---
+++ Data_Structure/Recursion/Sorting/Recursive_Bubble_Sort.java
@@ -0,0 +1,36 @@
+package Data_Structure.Sorting;
+import java.util.*;
+
+public class Recursive_Bubble_Sort {
+    static void bubble_sort(int[] arr, int n) {
+        //Base case: range == 1.
+        if (n == 1) return;
+        for (int j = 0; j <= n - 2; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+
+        //Range reduced after recursion
+        bubble_sort(arr, n - 1);
+
+    }
+    public static void main(String args[]) {
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+        System.out.println("Before Using Bubble Sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        bubble_sort(arr, n);
+
+        System.out.println("After bubble sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+} 
