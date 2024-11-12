1bd45ee6d7b5c3490ec87baf70ddb58a2c2716df
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2024-11-12 09:01:35 +0530

    Added the Recursion insertion sorting file

---
+++ Data_Structure/Recursion/Sorting/Recursion_Insertion_Sort.java
@@ -0,0 +1,36 @@
+package Data_Structure.Sorting;
+import java.util.*;
+
+public class Recursion_Insertion_Sort {
+    static void insertion_sort(int[] arr, int i, int n) {
+        // Base Case: i == n.
+        if (i == n) return;
+
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+
+        insertion_sort(arr, i + 1, n);
+
+    }
+    public static void main(String args[]) {
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+        System.out.println("Before Using insertion Sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        insertion_sort(arr, 0, n);
+        System.out.println("After insertion sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
