9cb71943342f525c695e074188a566a73e705f5b
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2024-11-13 16:25:53 +0530

    Problem no --> 26

---
+++ Leet_Code_Problems/RemoveDuplicate.java
@@ -0,0 +1,14 @@
+package Leet_Code_Problems;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
