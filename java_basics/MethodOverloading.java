99757f5deaae3eca032ad628e6eea41b249ecea7
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2024-12-11 12:38:02 +0530

    Added the file for methodoverloading

---
+++ java_basics/MethodOverloading.java
@@ -0,0 +1,26 @@
+package java_basics;
+
+class Calculator
+{
+	public int add(int n1, int n2, int n3)
+	{
+		return n1+n2+n3;
+	}
+	public int add(int n1, int n2)
+	{
+		return n1+n2;
+	}
+	public double add(double n1, int n2)
+	{
+		return n1+n2;
+	}
+}
+
+
+public class MethodOverloading {
+	public static void main(String[] args) {
+		Calculator obj=new Calculator();
+		int r1=obj.add(3,4);
+		System.out.println(r1);
+	}
+}
