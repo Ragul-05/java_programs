f678c32ec164511ec579283a364c62096a83fb4c
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2024-12-24 21:22:08 +0530

    Problem no --> 225

---
+++ Leet_Code_Problems/StackUsingQueue.java
@@ -0,0 +1,43 @@
+package Leet_Code_Problems;
+
+import java.util.LinkedList;
+import java.util.Queue;
+class MyStack {
+       private Queue<Integer> queue1;
+       private Queue<Integer> queue2;
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        queue2.add(x);
+        while (!queue1.isEmpty()) {
+            queue2.add(queue1.poll());
+        }
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+    
+    public int pop() {
+         return queue1.poll();
+    }
+    
+    public int top() {
+        return queue1.peek();
+    }
+    
+    public boolean empty() {
+         return queue1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
