05d1c4bcb666f60b248eb29a234fd2d0356ce5ec
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2025-01-08 22:28:56 +0530

    Problem no --> 122

---
+++ Leet_Code_Problems/StockII.java
@@ -0,0 +1,16 @@
+package Leet_Code_Problems;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+         int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+           
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+}
