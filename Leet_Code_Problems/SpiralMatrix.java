eec1f765049026349a6f21939edd7dd36d504c6a
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2025-01-16 14:30:41 +0530

    Problem no -->54

---
+++ Leet_Code_Problems/SpiralMatrix.java
@@ -0,0 +1,46 @@
+package Leet_Code_Problems;
+import java.util.*;
+class Solution {
+    public List<Integer> spiralOrder(int[][] mat) {
+         List<Integer> ans = new ArrayList<>();
+        
+        int n = mat.length; 
+        int m = mat[0].length; 
+        
+        
+        int top = 0, left = 0, bottom = n - 1, right = m - 1;
+
+       
+        while (top <= bottom && left <= right) {
+
+            
+            for (int i = left; i <= right; i++)
+                ans.add(mat[top][i]);
+
+            top++;
+
+          
+            for (int i = top; i <= bottom; i++)
+                ans.add(mat[i][right]);
+
+            right--;
+
+           
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--)
+                    ans.add(mat[bottom][i]);
+
+                bottom--;
+            }
+
+            
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    ans.add(mat[i][left]);
+
+                left++;
+            }
+        }
+        return ans;
+    }
+}
