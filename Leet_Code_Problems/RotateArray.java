b419ba4d5a924066362bc004abdb50abeea507a5
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2025-01-25 20:44:23 +0530

    Problem no --> 189

---
+++ Leet_Code_Problems/RotateArray.java
@@ -0,0 +1,19 @@
+package Leet_Code_Problems;
+
+class Solution{
+    public void rotate(int[] nums, int k){
+        k = k % nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+    }
+    public void reverse(int nums[], int s,int e){
+        while(s<e){
+            int temp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = temp;
+            s++;
+            e--;
+        }
+    }
+}
