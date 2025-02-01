3c91e4573503b1ca8ef7ff558b7d6cabad3396cd
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2025-02-01 23:02:13 +0530

    Problem no --> 31

---
+++ Leet_Code_Problems/NextPermutation.java
@@ -0,0 +1,39 @@
+package Leet_Code_Problems;
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int ind = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+        if (ind == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+        for (int i = n - 1; i > ind; i--) {
+            if (nums[i] > nums[ind]) {
+                swap(nums, i, ind);
+                break;
+            }
+        }
+        reverse(nums, ind + 1, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
